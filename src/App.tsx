f1dcdd62daade68bce3451df05a7e30f425cfbb6
AndriyBroda <broda.andriy2017@gmail.com>
2021-03-24 21:32:19 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Form, Formik } from 'formik';
 import { FormikInput } from './components/shared/formikAdapters';
 import { UserFormSchema } from './utils/validation-schemas';
-
+import { Category, deleteCategory, getCategories, postCategory } from './api';
 interface UserFormValues {
   firstName: string;
   lastName: string;
@@ -17,8 +17,43 @@ const defaultValues: UserFormValues = {
 };
 
 function App() {
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  const loadCategories = async () => {
+    const res = await getCategories();
+
+    setCategories(res.data.data);
+  };
+
+  useEffect(() => {
+    loadCategories();
+  }, []);
+
+  const sendCategory = async () => {
+    const res = await postCategory({
+      iconId: 2,
+      name: 'Automotive Parts',
+      description: 'Parts for auto'
+    });
+
+    console.log(res.data);
+  };
+
+  const removeCategory = async (id: number) => {
+    await deleteCategory(id);
+    loadCategories();
+  };
+
   return (
     <div>
+      <button onClick={sendCategory}>Create new category</button>
+      {categories.map(item => (
+        <div key={item.id}>
+          <span>{item.id}</span>
+          <span>{item.name}</span>
+          <button onClick={() => removeCategory(item.id)}>Delete</button>
+        </div>
+      ))}
       <Formik<UserFormValues>
         initialValues={defaultValues}
         validationSchema={UserFormSchema}
